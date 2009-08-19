06e83ec3492c3369bca3637fabe6c254d0c38631
mschaefer82 <mschaefer82@ea46ec8b-6419-0410-874a-eaae0b8e7eff>
2009-08-19 16:51:47 +0000

    - database config etc.

--- trunk/src/config/ProjectConfiguration.class.php
+++ trunk/src/config/ProjectConfiguration.class.php
@@ -1,12 +1,13 @@
 <?php
 
-require_once dirname(__FILE__) . '/../lib/vendor/symfony/lib/autoload/sfCoreAutoload.class.php';
+require_once 'C:\xampp\php\PEAR\symfony/autoload/sfCoreAutoload.class.php';
 sfCoreAutoload::register();
 
 class ProjectConfiguration extends sfProjectConfiguration
 {
   public function setup()
   {
+    // for compatibility / remove and enable only the plugins you want
     $this->enableAllPluginsExcept(array('sfPropelPlugin', 'sfCompat10Plugin'));
   }
 }
