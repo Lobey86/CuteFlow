417cd9672e9434cbc45ffc09b1296f400d523d56
mschaefer82 <mschaefer82@ea46ec8b-6419-0410-874a-eaae0b8e7eff>
2009-08-28 13:50:25 +0000

    - added actions for creating new userroles

--- trunk/src/lib/de/cf/userrolemanagement/credentialrolemanagement.php
+++ trunk/src/lib/de/cf/userrolemanagement/credentialrolemanagement.php
@@ -2,7 +2,7 @@
 /**
  * Class builds some kind of tree for the role management
  *
- * @author Manuel Sch�fer
+ * @author Manuel Schï¿½fer
  */
 class CredentialRolemanagement {
 
@@ -47,21 +47,33 @@ class CredentialRolemanagement {
 
                
                 $right = $item->getUserRight();
+                $id = $item->getId();
                 $result[$this->moduleCounter]['usermodule']['usergroup'][$this->groupCounter]['userright'][$this->rightCounter]['title'] = $right;
                 $result[$this->moduleCounter]['usermodule']['usergroup'][$this->groupCounter]['userright'][$this->rightCounter]['id'] =  $result[$this->moduleCounter]['usermodule']['usergroup'][$this->groupCounter]['id'] . '_userright_' . $right;
                 $result[$this->moduleCounter]['usermodule']['usergroup'][$this->groupCounter]['userright'][$this->rightCounter]['server_id'] = $right;
                 $result[$this->moduleCounter]['usermodule']['usergroup'][$this->groupCounter]['userright'][$this->rightCounter]['userright'] = $right;
                 $result[$this->moduleCounter]['usermodule']['usergroup'][$this->groupCounter]['userright'][$this->rightCounter]['name'] = $result[$this->moduleCounter]['usermodule']['usergroup'][$this->groupCounter]['id'];
                 $result[$this->moduleCounter]['usermodule']['usergroup'][$this->groupCounter]['userright'][$this->rightCounter]['parent'] = $this->checkParent($right);
-                $result[$this->moduleCounter]['usermodule']['usergroup'][$this->groupCounter]['userright'][$this->rightCounter++]['database_id'] = $item->getId();
+                $result[$this->moduleCounter]['usermodule']['usergroup'][$this->groupCounter]['userright'][$this->rightCounter]['parentelement'] = $result[$this->moduleCounter]['usermodule']['usergroup'][$this->groupCounter]['id'];
+                $result[$this->moduleCounter]['usermodule']['usergroup'][$this->groupCounter]['userright'][$this->rightCounter++]['database_id'] = $id;
                 
 
             }
+            #print_r ($result);die;
+            $result = $this->sortGroup($result);
             return $result;
         }
 
 
-
+        /**
+         * Function checks for equal group in the resultset and the current item
+         * if a group is already in the resultset, nothing is done.
+         * If group is not in resultset, tab is added.
+         *
+         * @param array $result, Resultset with the current data
+         * @param string $item, value of the current item
+         * @return string $item, retursn item or nothing.
+         */
         private function checkGroup($result, $item) {
             $flag = false;
             if ($this->firstRun == false OR $this->groupCounter > 0) {
@@ -119,7 +131,13 @@ class CredentialRolemanagement {
             }
         }
 
-
+        /**
+         * Checks for Parent item.
+         * Parent item must be: showModule
+         *
+         * @param String $item
+         * @return boolean, 1 for is Parent, 0 for non-parent
+         */
         private function checkParent($item) {
             if($item == 'showModule') {
                 return 1;
@@ -127,5 +145,33 @@ class CredentialRolemanagement {
             else {
                 return 0;
             }
+        }
+
+        /**
+         * Function sorts the builded tree, that module enabling is on top of an array.
+         * Necessarry that in ExtJS frontend, the fat black module name is on top.
+         *
+         * @param array $data
+         * @return array $data
+         */
+        private function sortGroup(array $data) {
+            $store_showModule = array();
+            $store_firtsElement = array();
+            
+            for($a=0;$a<count($data);$a++) {
+                for($b=0;$b<count($data[$a]['usermodule']['usergroup']);$b++) {
+                    for($c=0;$c<count($data[$a]['usermodule']['usergroup'][$b]['userright']);$c++) {
+                        if($data[$a]['usermodule']['usergroup'][$b]['userright'][$c]['server_id'] == 'showModule') {
+                            if($c > 0) {
+                                $store_showModule = $data[$a]['usermodule']['usergroup'][$b]['userright'][$c];
+                                $store_firtsElement = $data[$a]['usermodule']['usergroup'][$b]['userright'][0];
+                                $data[$a]['usermodule']['usergroup'][$b]['userright'][$c] = $store_firtsElement;
+                                $data[$a]['usermodule']['usergroup'][$b]['userright'][0] = $store_showModule;
+                            }
+                        }
+                    }
+                }
+            }
+            return $data;
         }
 }
