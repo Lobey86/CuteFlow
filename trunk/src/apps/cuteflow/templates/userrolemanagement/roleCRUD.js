694f60fa82fee8f0ea53a5ebee6511ee2746b13a
mschaefer82 <mschaefer82@ea46ec8b-6419-0410-874a-eaae0b8e7eff>
2009-08-29 10:26:15 +0000

    - added edit role functionality

--- trunk/src/apps/cuteflow/templates/userrolemanagement/roleCRUD.js
+++ trunk/src/apps/cuteflow/templates/userrolemanagement/roleCRUD.js
@@ -3,48 +3,58 @@ cf.RoleCRUD = function(){return {
 
 
 
-saveRole: function (new_flag) {
+deleteRole: function (id) {
+	cf.DeleteRoleWindow.init(id);
+},
+
+
+
 	
-	if (new_flag != 1) {
-		alert("edit");
-		// store edit.
-	}
-	
+
+saveRole: function (new_flag,id) {
 	var textfield = Ext.getCmp('userrole_title_id');
 	if(textfield.getValue() == '') {
 		cf.AddRoleTabpanel.theTabpanel.setActiveTab(0);
 		textfield.focus();
 	}
 	else {
-		Ext.Ajax.request({  
-			url : '<?php echo url_for('userrolemanagement/CheckForExistingRole')?>/description/' + textfield.getValue(),
-			success: function(objServerResponse){
-				if(objServerResponse.responseText == 1) {
-					cf.AddRoleTabpanel.theFormPanel.getForm().submit({
-						url: '<?php echo url_for('userrolemanagement/AddRole')?>',
-						method: 'POST',
-						success: function() {
-							cf.UserRoleGrid.theUserRoleStore.reload();
-							cf.AddRoleWindow.theAddRoleWin.hide();
-							cf.AddRoleWindow.theAddRoleWin.destroy();
-							try {
-								cf.UserRoleGrid.theUserRoleStore.load();
-							}
-							catch(e) {
-							}
-						}
-					});
+		if (new_flag != 1) { // edit role
+			
+			cf.AddRoleTabpanel.theFormPanel.getForm().submit({
+				url: '<?php echo url_for('userrolemanagement/EditRole')?>',
+				method: 'POST',
+				success: function() {
+					//cf.UserRoleGrid.theUserRoleStore.reload();
+					cf.AddRoleWindow.theAddRoleWin.hide();
+					cf.AddRoleWindow.theAddRoleWin.destroy();
 				}
-				else {
-					Ext.MessageBox.alert('<?php echo __('Error',null,'userrolemanagement'); ?>', '<?php echo __('Role is already existing',null,'userrolemanagement'); ?>');
-					cf.AddRoleTabpanel.theTabpanel.setActiveTab(0);
-					textfield.focus();
-					textfield.setValue();
+			});
+		}
+		else { // new role
+			Ext.Ajax.request({  
+				url : '<?php echo url_for('userrolemanagement/CheckForExistingRole')?>/description/' + textfield.getValue(),
+				success: function(objServerResponse){
+					if(objServerResponse.responseText == 1) {
+						cf.AddRoleTabpanel.theFormPanel.getForm().submit({
+							url: '<?php echo url_for('userrolemanagement/AddRole')?>',
+							method: 'POST',
+							success: function() {
+								cf.UserRoleGrid.theUserRoleStore.reload();
+								cf.AddRoleWindow.theAddRoleWin.hide();
+								cf.AddRoleWindow.theAddRoleWin.destroy();
+							}
+						});
+					}
+					else {
+						Ext.MessageBox.alert('<?php echo __('Error',null,'userrolemanagement'); ?>', '<?php echo __('Role is already existing',null,'userrolemanagement'); ?>');
+						cf.AddRoleTabpanel.theTabpanel.setActiveTab(0);
+						textfield.focus();
+						textfield.setValue();
+					}
 				}
-			}
-		});
+			});
+		}
 	}
-	
 }
 
 
