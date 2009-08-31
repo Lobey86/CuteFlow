b9810abfdb8fd4c4e68053268e630396a1a78821
mschaefer82 <mschaefer82@ea46ec8b-6419-0410-874a-eaae0b8e7eff>
2009-08-31 14:23:20 +0000

    - added language selection for EXTjs

--- src/apps/cuteflow/modules/layout/actions/actions.class.php
+++ src/apps/cuteflow/modules/layout/actions/actions.class.php
@@ -1,33 +1,35 @@
 <?php
 
 /**
- * layout actions.
- *
- * @package    cf
- * @subpackage layout
- * @author     Your name here
- * @version    SVN: $Id: actions.class.php 12479 2008-10-31 10:54:40Z fabien $
- */
-class layoutActions extends sfActions
-{
- /**
-  * Load Userrights and User Id to Sesseion.
-  *
-  * @param sfRequest $request A request object
-  */
-  public function executeIndex(sfWebRequest $request)
-  {
-      // stores usersettings to session
-      $userSettings = Doctrine::getTable('UserSetting')->find($this->getUser()->getAttribute('id'));
-      $this->getUser()->setAttribute('userSettings', $userSettings);
+* layout actions.
+*
+* @package    cf
+* @subpackage layout
+* @author     Your name here
+* @version    SVN: $Id: actions.class.php 12479 2008-10-31 10:54:40Z fabien $
+*/
+class layoutActions extends sfActions {
+    /**
+     *
+    * Load Userrights and User Id to Sesseion.
+    *
+    * @param sfRequest $request A request object
+    */
+    public function executeIndex(sfWebRequest $request) {
+        // stores usersettings to session
+        $userSettings = Doctrine::getTable('UserSetting')->find($this->getUser()->getAttribute('id'));
+        $this->getUser()->setAttribute('userSettings', $userSettings);
 
 
+ 
+
+ 
       
        // LOAD USERRIGHTS HERE
       // stores userRigts to session
       //$userRole = Doctrine::getTable('UserRole')->find($this->getUser()->getAttribute('id'));
       //$this->getUser()->setAttribute('userRole', $userRole);
 
-      return sfView::SUCCESS;
-  }
+        return sfView::SUCCESS;
+    }
 }
