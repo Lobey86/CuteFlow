6688c50f2057c6bc509ef24c438968974a5571c9
mschaefer82 <mschaefer82@ea46ec8b-6419-0410-874a-eaae0b8e7eff>
2009-09-02 15:36:05 +0000

    - added dynamic menue

---
+++ trunk/src/apps/cuteflow/modules/menue/actions/actions.class.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+* menue actions.
+*
+* @package    cf
+* @subpackage menue
+* @author     Your name here
+* @version    SVN: $Id: actions.class.php 12479 2008-10-31 10:54:40Z fabien $
+*/
+class menueActions extends sfActions {
+    /**
+    * Executes index action
+    *
+    * @param sfRequest $request A request object
+    */
+    public function executeIndex(sfWebRequest $request) {
+        $this->forward('default', 'module');
+    }
+
+
+    public function executeLoadMenue(sfWebRequest $request) {
+        $menueObject = new Menue();
+
+        $result = Doctrine_Query::create()
+            ->from('Credential c')
+            ->orderby('c.usermodule asc,c.usergroup asc, c.id asc')
+            ->execute();
+        
+        $menueObject->setRecords($result);
+        $menueObject->setUserright($this->getUser()->getAttribute('credential'));
+        $menueObject->setContext($this->getContext());
+
+        $json_result = $menueObject->buildTree();
+        $this->renderText('{"result":'.json_encode($json_result).'}');
+     
+        return sfView::NONE;
+    }
+
+
+
+}
