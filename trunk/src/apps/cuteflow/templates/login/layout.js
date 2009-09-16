36cd95a9e62944f990c8dcb180de9c33f10b072a
mschaefer82 <mschaefer82@ea46ec8b-6419-0410-874a-eaae0b8e7eff>
2009-09-16 13:54:31 +0000

    - changed login from get to post

--- trunk/src/apps/cuteflow/templates/login/layout.js
+++ trunk/src/apps/cuteflow/templates/login/layout.js
@@ -15,9 +15,10 @@ cf.Layout = function(){return {
 		
 		cf.Textfield.thePanel.add(cf.Textfield.theUsernameField);
 		cf.Textfield.thePanel.add(cf.Textfield.theUserpasswordField);
+		cf.Textfield.thePanel.add(cf.Textfield.theHiddenField);
 		cf.Textfield.thePanel.add(cf.ComboBox.theComboBox);
 		cf.Window.theWindow.add(cf.Textfield.thePanel);
-		cf.Window.theWindow.add(cf.Textfield.theHiddenField);
+		//cf.Window.theWindow.add(cf.Textfield.theHiddenField);
 
 		cf.Window.theWindow.show();
 	}
