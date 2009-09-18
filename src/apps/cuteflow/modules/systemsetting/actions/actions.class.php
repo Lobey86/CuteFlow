b0c4760823167bd26354961a6546ab08077d532b
mschaefer82 <mschaefer82@ea46ec8b-6419-0410-874a-eaae0b8e7eff>
2009-09-18 17:49:48 +0000

    - added systemsettings tab - added save functionlity for email settings

--- src/apps/cuteflow/modules/systemsetting/actions/actions.class.php
+++ src/apps/cuteflow/modules/systemsetting/actions/actions.class.php
@@ -30,7 +30,7 @@ class systemsettingActions extends sfActions {
                 ->select('ec.*')
                 ->from('EmailConfiguration ec')
                 ->fetchArray();
-        
+        $email[0]['smtpencryption'] =  $email[0]['smtpencryption'] == '' ? 'NONE' :  $email[0]['smtpencryption'];
         $this->renderText('{"email":'.json_encode($email[0]).'}');
         return sfView::NONE;
     }
@@ -55,6 +55,9 @@ class systemsettingActions extends sfActions {
         }
         
         if (isset($data['emailtab_emailtype'])) { // store Email tab
+        
+        $data['emailtab_encryption'] = $data['emailtab_encryption'] == 'NONE' ? '' : $data['emailtab_encryption'];
+
         Doctrine_Query::create()
             ->update('EmailConfiguration ec')
             ->set('ec.systemreplyaddress', '?', $data['emailtab_systemreplyaddress'])
