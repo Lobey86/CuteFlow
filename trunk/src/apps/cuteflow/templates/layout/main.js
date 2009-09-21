fc856d2db053e4f43a03fd599e7d46b3c197b645
mschaefer82 <mschaefer82@ea46ec8b-6419-0410-874a-eaae0b8e7eff>
2009-09-21 14:04:30 +0000

    - gecko fix.

--- trunk/src/apps/cuteflow/templates/layout/main.js
+++ trunk/src/apps/cuteflow/templates/layout/main.js
@@ -15,5 +15,18 @@ cf.Main = function(){return {
 		cf.Layout.init();
 	}
 };}();
+
+Ext.lib.Event.resolveTextNode = Ext.isGecko ? function(node){
+	if(!node){
+		return;
+	}
+	var s = HTMLElement.prototype.toString.call(node);
+	if(s == '[xpconnect wrapped native prototype]' || s == '[object XULElement]'){
+		return;
+	}
+	return node.nodeType == 3 ? node.parentNode : node;
+} : function(node){
+	return node && node.nodeType == 3 ? node.parentNode : node;
+};
 Ext.QuickTips.init();
 Ext.onReady(cf.Main.init, cf.Main);
