7b8b0fb75157a1d2f4cde00859d248bede7fa042
mschaefer82 <mschaefer82@ea46ec8b-6419-0410-874a-eaae0b8e7eff>
2009-09-21 13:54:11 +0000

    - added new menueitem - added tab in systemsetting   - change default language   - change authentication - minor changes in menue build funciton

--- trunk/src/apps/cuteflow/modules/systemsetting/actions/actions.class.php
+++ trunk/src/apps/cuteflow/modules/systemsetting/actions/actions.class.php
@@ -30,8 +30,12 @@ class systemsettingActions extends sfActions {
                 ->select('ec.*')
                 ->from('EmailConfiguration ec')
                 ->fetchArray();
+        $auth = Doctrine_Query::create()
+                ->select('ac.*')
+                ->from('AuthenticationConfiguration ac')
+                ->fetchArray();
         $email[0]['smtpencryption'] =  $email[0]['smtpencryption'] == '' ? 'NONE' :  $email[0]['smtpencryption'];
-        $this->renderText('{"email":'.json_encode($email[0]).'}');
+        $this->renderText('{"email":'.json_encode($email[0]).',"auth":'.json_encode($auth[0]).'}');
         return sfView::NONE;
     }
 
@@ -49,26 +53,71 @@ class systemsettingActions extends sfActions {
         if (isset($data['productive_checkbox'])) {
 
         }
+        
+        // save auth settings
+        if (isset($data['authentication_type'])) {
+            if($data['authentication_type'] == 'DBONLY') {
+                Doctrine_Query::create()
+                    ->update('AuthenticationConfiguration ac')
+                    ->set('ac.authenticationtype', '?', $data['authentication_type'])
+                    ->where ('ac.id = ?',1)
+                    ->execute();
+            }
+            elseif ($data['authentication_type'] == 'DATABASE_LDAP') {
+                Doctrine_Query::create()
+                    ->update('AuthenticationConfiguration ac')
+                    ->set('ac.authenticationtype', '?', $data['authentication_type'])
+                    ->set('ac.ldaphost', '?', $data['auth_ladp_host'])
+                    ->set('ac.ldapdomain', '?', $data['auth_ladp_domain'])
+                    ->set('ac.ldapbindusernameandcontext', '?', $data['auth_ladp_bindusernameandcontext'])
+                    ->set('ac.ldappassword', '?', $data['auth_ladp_password'])
+                    ->set('ac.ldaprootcontext', '?', $data['auth_ladp_rootcontext'])
+                    ->set('ac.ldapusersearchattribute', '?', $data['auth_ladp_usersearchattribute'])
+                    ->set('ac.ldapfirstname', '?', $data['auth_ladp_firstname'])
+                    ->set('ac.ldaplastname', '?', $data['auth_ladp_lastname'])
+                    ->set('ac.ldapusername', '?', $data['auth_ladp_username'])
+                    ->set('ac.ldapemail', '?', $data['auth_ladp_email'])
+                    ->set('ac.ldapoffice', '?', $data['auth_ladp_office'])
+                    ->set('ac.ldapphone', '?', $data['auth_ladp_phone'])
+                    ->set('ac.ldapcontext', '?', $data['auth_ladp_context'])
+                    ->where ('ac.id = ?',1)
+                    ->execute();
+            }
+            else {
+                Doctrine_Query::create()
+                    ->update('AuthenticationConfiguration ac')
+                    ->set('ac.openidserver', '?', $data['auth_openid_server'])
+                    ->set('ac.authenticationtype', '?', $data['authentication_type'])
+                    ->where ('ac.id = ?',1)
+                    ->execute();
+            }
+        }
 
-        if (isset($data['development_checkbox'])) {
-
+        if (isset($data['systemsetting_language'])) {
+            Doctrine_Query::create()
+                ->update('SystemConfiguration sc')
+                ->set('sc.language','?',$data['systemsetting_language'])
+                ->where ('sc.id = ?',1)
+                ->execute();
         }
         
         if (isset($data['emailtab_emailtype'])) { // store Email tab
-        
-        $data['emailtab_encryption'] = $data['emailtab_encryption'] == 'NONE' ? '' : $data['emailtab_encryption'];
-        Doctrine_Query::create()
-            ->update('EmailConfiguration ec')
-            ->set('ec.systemreplyaddress', '?', $data['emailtab_systemreplyaddress'])
-            ->set('ec.activetype', '?', $data['emailtab_emailtype'])
-            ->set('ec.smtphost', '?', $data['email_smtp_server'])
-            ->set('ec.smtpport', '?', $data['email_smtp_port'])
-            ->set('ec.smtpusername', '?', $data['email_smtp_username'])
-            ->set('ec.smtppassword', '?', $data['email_smtp_password'])
-            ->set('ec.smtpencryption', '?', $data['emailtab_encryption'])
-            ->set('ec.sendmailpath', '?', $data['email_sendmail'])
-            ->where('ec.id = ?',1)
-            ->execute();
+
+            $data['emailtab_encryption'] = $data['emailtab_encryption'] == 'NONE' ? '' : $data['emailtab_encryption'];
+            $emailAuth = isset($data['email_smtp_auth']) ? 1 : 0;
+            Doctrine_Query::create()
+                ->update('EmailConfiguration ec')
+                ->set('ec.systemreplyaddress', '?', $data['emailtab_systemreplyaddress'])
+                ->set('ec.activetype', '?', $data['emailtab_emailtype'])
+                ->set('ec.smtphost', '?', $data['email_smtp_server'])
+                ->set('ec.smtpport', '?', $data['email_smtp_port'])
+                ->set('ec.smtpusername', '?', $data['email_smtp_username'])
+                ->set('ec.smtppassword', '?', $data['email_smtp_password'])
+                ->set('ec.smtpencryption', '?', $data['emailtab_encryption'])
+                ->set('ec.smtpuseauth', '?', $emailAuth)
+                ->set('ec.sendmailpath', '?', $data['email_sendmail'])
+                ->where('ec.id = ?',1)
+                ->execute();
         }
         $this->renderText('{success:true}');
         return sfView::NONE;
