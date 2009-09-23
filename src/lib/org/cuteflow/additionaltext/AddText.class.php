a22bd09d3e198e421f7e0ef07d2072fb50bef409
mschaefer82 <mschaefer82@ea46ec8b-6419-0410-874a-eaae0b8e7eff>
2009-09-23 20:23:55 +0000

    - added layout changes

--- src/lib/org/cuteflow/additionaltext/AddText.class.php
+++ src/lib/org/cuteflow/additionaltext/AddText.class.php
@@ -8,7 +8,7 @@ class AddText {
 
 
     public function  __construct() {
-        
+         sfLoader::loadHelpers('I18N');
     }
 
     /**
@@ -17,13 +17,13 @@ class AddText {
      * @param Doctrine_Collection $data, all records for grid
      * @return array $resultset, resultset.
      */
-    public function buildAllText(Doctrine_Collection $data) {
+    public function buildAllText(Doctrine_Collection $data, sfContext $context) {
         $a = 0;
         $result = array();
         foreach($data as $item) {
             $result[$a]['#'] = $a+1;
             $result[$a]['title'] = $item->getTitle();
-            $result[$a]['contenttype'] = $item->getContenttype();
+            $result[$a]['contenttype'] = $context->getI18N()->__($item->getContenttype(),null,'additionaltext');
             $result[$a]['isactive'] = $item->getIsactive();
             $result[$a++]['id'] = $item->getId();
         }
